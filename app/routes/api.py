ebcc36ec469e26adc95db1e2f3085e9ca45b65a1
Logan Davis <Ldavis96@live.com>
2022-05-05 19:02:36 -0400

    updated def signup route to include bracket notation and return JSON, fixed error

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -7,11 +7,17 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
   data = request.get_json()
-  
+  db = get_db()
+
+  # create a new user
   newUser = User(
-    username = data.username,
-    email = data.email,
-    password = data.password
+    username = data['username'],
+    email = data['email'],
+    password = data['password']
   )
 
-  return ''
+  # save in database
+  db.add(newUser)
+  db.commit()
+
+  return jsonify(id = newUser.id)
